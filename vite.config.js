b28fdcd6766d0358ae406a54f8edf9647a5a9e6b
Aswith-varma <aswithvarma54@gmail.com>
2025-07-31 11:37:04 +0530

    Add crypto polyfill to fix build error in GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import NodePolyfills from 'rollup-plugin-node-polyfills';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      crypto: 'rollup-plugin-node-polyfills/polyfills/crypto',
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [NodePolyfills()],
+    },
+  },
+});
